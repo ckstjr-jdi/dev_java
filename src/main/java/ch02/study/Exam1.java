7113270fe9cc2c833f7a57d7a6b627fdc7be1b52
찬돌 <a50668151@gmail.com>
2025-11-28 17:47:42 +0900

    원본과 복사번(참조형변수)

---
+++ src/main/java/ch02/study/Exam1.java
@@ -0,0 +1,16 @@
+package ch02.study;
+/*
+JVM이 제공하는 API가 있다.
+실행을 위해서는 main메서드 선언하기 - 실행할 수 있다. - 콘솔에 출력해 볼 수 있다.
+파일명은 Exam1.java로 만들고 컴파일을 거치면 Exam1.class가 만들어진다.
+컴파일이란 고급언어(.java)->저급언어(.class)로 바꾸어 준다.
+컴파일 과정에서 문법에러가 발견되면 XXX.class파일이 만들어지지 않음.
+클래스 선언 후에 좌중괄호와 우중괄호로 스코프를 정해줌.
+여러 행을 작성하더라도 항상 main메서드가 제일 먼저 실행됨. - 개발자가 호출하는 메서드가 아님
+-> 나중에는 재사용성이나 이식성을 
+ */
+public class Exam1 {
+    public static void main(String[] args) {
+
+    }//end of main
+}//end of Exam1

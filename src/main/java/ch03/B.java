1c28ff31ca69c9b0b9d8d1e784464e7f2d69c96d
찬돌 <a50668151@gmail.com>
2025-12-01 16:42:37 +0900

    윈도우 창에서 연산하기

---
+++ src/main/java/ch03/B.java
@@ -0,0 +1,12 @@
+package ch03;
+
+public class B {
+    //전역변수 선언 위치에서는 선언과 초기화를 분리하지 않습니다.
+    //지역변수는 선언하기와 초기화 하는 것을 분리하여 작성할 수 있음.
+    int i;
+    //i = 1;
+    public static void main(String[] args) {
+        int i;
+        i = 5;
+    }
+}

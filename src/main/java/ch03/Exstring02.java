99d93229124bcac75990b62c8ea88a327d2771e1
찬돌 <a50668151@gmail.com>
2025-12-02 12:55:24 +0900

    String

---
+++ src/main/java/ch03/Exstring02.java
@@ -0,0 +1,17 @@
+package ch03;
+
+public class Exstring02 {
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "Java";
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.hashCode() == s2.hashCode());
+        System.out.println(s1.hashCode());
+    }
+}
+/*
+String클래스가 제공하는 hashCode()는 내용 기반으로
+같은 내용이면 같은 해시값을 반환함.
+즉, 객체가 서로 다르더라도 내용이 같으면 같은 hashCode를 만든다.
+ */
---
+++ src/main/java/ch03/Exstring04.java
@@ -0,0 +1,15 @@
+package ch03;
+// String - 읽기 전용 클래스 이다.
+// 원본이 바뀌지 않음. - 불변객체이다.
+public class Exstring04 {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        System.out.println(System.identityHashCode(s1));
+        s1 = s1 + " World!!!";
+        System.out.println(System.identityHashCode(s1));
+        System.out.println(s1);
+        s1 = s1 + " Java";
+        System.out.println(System.identityHashCode(s1));
+        //s1 객체가 3개가 만들어 졌다.
+    }
+}

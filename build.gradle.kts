a7c5f20dd2fb14d9d8a846c76d92fca7150cb09e
찬돌 <a50668151@gmail.com>
2025-12-03 18:29:33 +0900

    자바 & 오라클

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,12 +11,12 @@ repositories {
 }
 
 dependencies {
+    implementation("com.oracle.database.jdbc:ojdbc8:23.26.0.0.0")
+    implementation("com.oracle.database.nls:orai18n:23.26.0.0.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation(kotlin("stdlib-jdk8"))
 }
-
 tasks.test {
     useJUnitPlatform()
 }

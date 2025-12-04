f311fd80399c97e943ede6e41c20bda1112b2c48
찬돌 <a50668151@gmail.com>
2025-12-04 18:16:17 +0900

    오라클 자바 연동

---
+++ src/main/java/ch05/aobject2/design/JFrame01Event.java
@@ -0,0 +1,22 @@
+package ch05.aobject2.design;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+//Spring Boot에서는 개발자가 직접 인스턴스화 하지 않습니다.
+//생성자는 리턴타입이 올 수 없다.
+public class JFrame01Event implements ActionListener {
+    JFrame01View jv = null;
+    public JFrame01Event(JFrame01View jv){
+        System.out.println("JFrame01Event 생성자 호출 성공");
+        this.jv = jv;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String commend = e.getActionCommand();
+        System.out.println(commend);//클릭할 때 마다 감지되어 해당 메서드를 호출함
+        if("조회".equals(commend)){
+            System.out.println("조회버튼이 클릭");
+            jv.jlogic.gerList();
+        }
+    }
+}
